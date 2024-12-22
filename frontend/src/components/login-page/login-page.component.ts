89c414ce0185dfedb4ea96f8be46aa4c62f85679
Rofieda <mr_maamar@esi.dz>
2024-12-22 23:43:02 +0500

    integrate login functionality with token

--- frontend/src/components/login-page/login-page.component.ts
+++ frontend/src/components/login-page/login-page.component.ts
@@ -2,31 +2,47 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
-import { Router } from '@angular/router';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css'],
-  imports: [CommonModule, FormsModule,HeaderComponent],
+  imports: [CommonModule, FormsModule, HeaderComponent, HttpClientModule],
 })
-
-
 export class LoginPageComponent {
   email: string = '';
   password: string = '';
 
-  onSubmit(): void {
-    console.log('Email:', this.email);
-    console.log('Password:', this.password);
-  }
-  
-  constructor(private router: Router) { }
+  constructor(private http: HttpClient) {}
 
-  navigateToLogin() {
-    this.router.navigate(['/recherche']);
-  }
+  // Inside your Angular login response handling
+onSubmit() {
+  const loginData = {
+    email: this.email,
+    password: this.password,
+  };
+
+  this.http.post('http://localhost:8000/accounts/login/', loginData, { withCredentials: true }).subscribe(
+    (response: any) => {
+      console.log('Login successful:', response);
+
+      // Save the access token in localStorage
+      localStorage.setItem('accessToken', response.access);
+      localStorage.setItem('userRole', response.role);
+      console.log('User role saved in localStorage:', response.role);
+      
+      // Tokens are saved automatically in cookies by the backend ( login api )
+      console.log('Tokens saved in cookies');
+      
+      
+    },
+    (error) => {
+      console.error('Login failed:', error);
+      // Handle error response (e.g., show error message)
+    }
+  );
 }
 
-
+}
