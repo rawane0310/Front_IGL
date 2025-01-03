e89373f4c0c927812533007f38249a8fc1224838
belaid1meriem <mm_belaid@esi.dz>
2025-01-03 16:21:09 +0100

    Added nom patient in the navbar

--- frontend/src/components/menu-dpi/menu-dpi.component.ts
+++ frontend/src/components/menu-dpi/menu-dpi.component.ts
@@ -1,8 +1,10 @@
-import { Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { NgIf } from '@angular/common';
-import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterModule, RouterOutlet } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {faUser, faNotesMedical, faUserNurse, faChartBar, faBone} from '@fortawesome/free-solid-svg-icons'
+import { UserIndicatorsServiceService } from '../../services/user-indicators-service.service';
+import axios from 'axios';
 
 @Component({
   selector: 'app-menu-dpi',
@@ -19,4 +21,49 @@ export class MenuDpiComponent {
  faBone= faBone;
 
  dpiId = input.required()
+ patientName = signal<string>('Patient')
+
+  capitalizeFirstLetter(input: string): string {
+    if (!input) return input; // Handle empty strings
+    return input.charAt(0).toUpperCase() + input.slice(1);
+  }
+
+ constructor(private route: ActivatedRoute, public userIndicatorService: UserIndicatorsServiceService){}
+ async ngOnInit(): Promise<void> {
+  try {
+    this.userIndicatorService.loadingData.set({
+      ...this.userIndicatorService.loadingData(),
+      isLoading: true
+    })
+
+    
+    const response = await axios.get('http://localhost:8000/dpi/search-patient/'+this.dpiId()+'/',{
+      headers: {
+        'Authorization': 'Bearer '+ localStorage.getItem('accessToken'),
+        }  
+    });
+
+    console.log(response.data);
+
+    if (response.status === 200) this.patientName.set( this.capitalizeFirstLetter(response.data.nom)+' '+this.capitalizeFirstLetter(response.data.prenom));
+    else this.userIndicatorService.errorData.set({
+      isError: true,
+      errorMessage: 'Erreur lors de la récupération des données du patient'
+    })
+
+  }
+  catch (error) {
+    console.log(error);
+    this.userIndicatorService.errorData.set({
+      isError: true,
+      errorMessage: 'Erreur lors de la récupération des données du patient'
+    })
+  }
+  finally{
+    this.userIndicatorService.loadingData.set({
+      ...this.userIndicatorService.loadingData(),
+      isLoading: false
+    })
+  }
+ }
 }
